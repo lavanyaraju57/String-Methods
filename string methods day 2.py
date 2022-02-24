6037493a5ce483efb429d6a5eedcf6dfe7aaf2b8
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-02-24 10:09:08 +0530

    Add files via upload

---
+++ string methods day 2.py
@@ -0,0 +1,79 @@
+'''is identifier'''
+
+# w="_3hello"
+# print(w.isidentifier())
+
+'''find'''
+
+# b="python programming"
+# print(b.find('p'))
+# print(b.find('e'))
+
+'''rfind'''
+
+# b='python programming'
+# print(b.rfind('g'))
+# print(len(b))
+
+'''count'''
+
+# v="python programming"
+
+# print(v.count("p"))
+# print(v.count('m'))
+# print(v.count('e'))
+
+'''replace'''
+
+# h="python programming"
+# print(h.replace('course','programming'))
+
+# g='core python programming core'
+# print(g.replace('core','2'))
+# print(g.replace('o','k',2))
+# print(g.replace('o','k',(2)))
+
+
+'''strip,lstrip,rstip'''
+
+# d="      python programming  .     "
+# print(d.strip())
+# print(d.lstrip())
+# print(d.rstrip())
+
+# e="    hello python    ".strip()
+# print(len(e))
+
+'''join'''
+
+# a="hello","python",'pro'
+# print(a,type(a))
+
+# b=' world '.join(a)
+# print(b)
+# # print("hello","python","pro",sep=" world ")
+# c="hey"
+# d="*".join(c)
+# print(d)
+# e="hey",""
+# f="*".join(e)
+# print(f)
+
+'''remove prefix'''
+
+# c="python programming"
+# print(c.removeprefix('p'))
+# print(c.removeprefix('py'))
+# print(c.removeprefix('python'))
+
+'''removesuffix'''
+
+c="python programming"
+print(c.removesuffix('g'))
+print(c.removesuffix('ing'))
+print(c.removesuffix('py'))
+
+
+
+
+
