f50ccc1777d6fbada085972a9c8f472d9a33a7c7
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-02-28 11:25:23 +0530

    Add files via upload

---
+++ string methods day 3.py
@@ -0,0 +1,109 @@
+'''isalnum'''
+
+# a="hello python"
+# print(a.isalnum())
+
+# a="hellopython"
+# print(a.isalnum())
+
+# a="hello.python"
+# print(a.isalnum())
+
+# a='hellopython39'
+# print(a.isalnum())
+
+'''isalpha'''
+
+# b='core python'
+# print(b.isalpha())
+
+# b="corepython"
+# print(b.isalpha())
+
+# b='corepython39'
+# print(b.isalpha())
+
+# b="6"
+# print(b.isalpha())
+
+# b="M"
+# print(b.isalpha())
+
+# b=""
+# print(b.isalpha())
+
+# b=' '
+# print(b.isalpha())
+
+'''isascii'''
+
+# v="august python"
+# print(v.isascii())
+
+# b=chr(0)
+# c=chr(32)
+# print(b.isspace())
+# print(c.isspace())
+# print("hello"+chr(32)+"python")
+# print("hello"+' '+"python")
+# print("hello","python",sep=' ')
+# print("hello","python")
+
+# print(chr(32)+"hey")
+# print(chr(0)+"hey")
+
+# print(ord("a"))
+# print(ord("z"))
+# print(ord("A"))
+# print(ord("Z"))
+# print(ord("0"))
+# print(ord("9"))
+# print(ord(" "))
+# print(ord("!"))
+# print(ord(''))
+
+"""TO FIND THE ASCII VALUE"""
+
+# print(chr(0))
+# print(chr(97))
+# print(chr(65))
+# print(chr(30))
+# print(chr(31))
+# print(chr(32))
+# print(chr(45))
+# print(chr(120))
+
+# v=bin(5)
+# print(v,type(v))
+# h=0b101
+# print(h,type(h))
+# j=0B101
+# print(j,type(j))
+
+# print(bin(23))
+# print(0b10111)
+# print(0B10111)
+
+# f=oct(11)
+# print(f,type(f))
+# print(0o13)
+# print(0O13)
+
+# w=hex(18)
+# print(w,type(w))
+# print(0x12)
+# print(0X12)
+# print(0xA)
+# print(0xD)
+# print(0xF)
+
+a=65
+print(a,type(a))
+print(ord('1'))
+print(chr(0o142))
+print(chr(0o41))
+print(ord('A'))
+
+
+
+
